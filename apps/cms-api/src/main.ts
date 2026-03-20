9a0b3b33be927b886ca50ecbe8ab253be1c13386
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-03-20 20:04:20 -0300

    feat: added swagger base config

--- apps/cms-api/src/main.ts
+++ apps/cms-api/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('CMS API')
+    .setDescription('API Documentation')
+    .setVersion('1.0')
+    .addTag('CMS')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+  
   app.enableCors();
   await app.listen(3000);
 }
