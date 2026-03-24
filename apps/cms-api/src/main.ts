792fe51509255c6105d276f56e04bf889bbeec2c
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-03-24 20:15:51 -0300

    feat: migrations to db

--- apps/cms-api/src/main.ts
+++ apps/cms-api/src/main.ts
@@ -3,10 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import cookieParser from "cookie-parser"
+import globalEnv from '@repo/env';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser())
+
+  console.log("Env", globalEnv.DIRECT_URL )
+
   const config = new DocumentBuilder()
     .setTitle('CMS API')
     .setDescription('API Documentation')
--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,22 +1,22 @@
-import {z} from "zod";
-import * as dotenv from "dotenv"
+import { z } from 'zod';
+import * as dotenv from 'dotenv';
 
-dotenv.config({path: "../../.env"});
+dotenv.config({ path: '../../.env' });
 
 const schemaEnv = z.object({
-    //environment
-    NODE_ENV: z.string().default("development"),
-    //cms-api
-    JWT_SECRET: z.string(),
-    PORT: z.string().default("3000"),
-    //cms-app
-    NEXT_PUBLIC_API_URL: z.string(),
-    
-    //Prisma
-    PRISMA_LOGS: z.string(),
-    DATABASE_URL: z.string(),
-})
+  //environment
+  NODE_ENV: z.string().default('development'),
+  //cms-api
+  JWT_SECRET: z.string(),
+  PORT: z.string().default('3000'),
+  //cms-app
+  NEXT_PUBLIC_API_URL: z.string(),
 
+  //Prisma
+  PRISMA_LOGS: z.string(),
+  DATABASE_URL: z.string(),
+  DIRECT_URL: z.string(),
+});
 
-const globalEnv = schemaEnv.parse(process.env) 
+const globalEnv = schemaEnv.parse(process.env);
 export default globalEnv;
