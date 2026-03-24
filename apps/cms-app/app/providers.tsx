3fbe60671f701ec9b47e4d2c66df8bc90c1281dc
LautaroLD <duranlautarogabriel@gmail.com>
2026-03-24 19:11:33 -0300

    feat: agregar QueryClient y react-hook-form, actualizar dependencias en providers y package.json

--- apps/cms-app/app/providers.tsx
+++ apps/cms-app/app/providers.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { SessionProvider } from 'next-auth/react';
-
-export function Providers({ children }: { children: React.ReactNode }) {
+const queryClient = new QueryClient();
+export function Providers({ children }: { children: React.ReactNode; }) {
   return (
     <SessionProvider>
-      {children}
+      <QueryClientProvider client={queryClient}>
+        {children}
+      </QueryClientProvider>
     </SessionProvider>
   );
 }
