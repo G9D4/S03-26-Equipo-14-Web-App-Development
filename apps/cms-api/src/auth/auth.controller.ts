82c75e7f6ab13d839fcb7787e61aa27e76ca8e3a
francocasafus22 <francocasafus55@gmail.com>
2026-03-24 00:09:40 -0300

    add: auth login

--- apps/cms-api/src/auth/auth.controller.ts
+++ apps/cms-api/src/auth/auth.controller.ts
@@ -10,7 +10,22 @@ export class AuthController {
         private readonly authService: AuthService
     ){}
 
-    
+    @Post("login")
+    async login(@Body() loginDto : {email: string, password: string}, @Res({passthrough: true}) res: Response){
+        const {access_token} = await this.authService.login(loginDto);
+        // Dev config
+        res.cookie("CMS_ACCESS_TOKEN", access_token, {
+            httpOnly: true,
+            secure: false,
+            sameSite: "lax",
+            maxAge: 1000*60*60*24,
+            path: "/"
+        })
+
+        return {
+            message: "Login successful"
+        }
+    }
 
     @Get("me")
     async me(@GetUser() user: {userId: string}){
