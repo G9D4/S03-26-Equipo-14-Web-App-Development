8a609807750dc26841dea24cfc538c4d920ec32c
EmanuelFuneS <emanuel.ignacio.funes@gmail.com>
2026-03-27 20:58:34 -0300

    feat: added response interceptor

---
+++ apps/cms-api/src/common/interceptors/response.interceptor.ts
@@ -0,0 +1,24 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class ResponseInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(
+      map((data) => ({
+        success: true,
+        status: data.status,
+        message: data?.message ?? 'ok',
+        data: data?.data ?? data,
+      })),
+    );
+  }
+}
