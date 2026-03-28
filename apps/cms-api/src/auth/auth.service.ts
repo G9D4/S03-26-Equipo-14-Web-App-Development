d21230f204a4c8c19ed4be524dead7d389046c30
francocasafus22 <francocasafus55@gmail.com>
2026-03-27 22:17:22 -0300

    fix import

--- apps/cms-api/src/auth/auth.service.ts
+++ apps/cms-api/src/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { compare, hash } from 'bcrypt';
 import {
